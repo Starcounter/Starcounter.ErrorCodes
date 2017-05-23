160dde0a8adcad2b22bd31cb5203c327d8b7cce9
Christian Holmstrand <christian.holmstrand@starcounter.com>
2017-05-23 09:23:36 +0200

    Updated push script with url to myget and added check for API key

--- build/push.bat
+++ build/push.bat
@@ -1,2 +1,20 @@
 @ECHO OFF
-dotnet nuget push --source \\127.0.0.1\share ..\artifacts\
+
+:: SET "NUGET_URL=https://www.myget.org/F/starcounter/api/v2/package"
+:: SET "NUGET_SS_URL=https://www.myget.org/F/starcounter/symbols/api/v2/package"
+
+SET "NUGET_URL=\\127.0.0.1\Share"
+:: SET "NUGET_SS_URL=\\127.0.0.1\Share"
+
+IF "%StarcounterNuGetKeyFull%"=="" (
+	ECHO StarcounterNuGetKeyFull is not set
+	EXIT /b 990
+)
+
+dotnet nuget push --source %NUGET_URL% --api-key %StarcounterNuGetKeyFull%  ..\artifacts\
+
+IF ERRORLEVEL 1 (
+    ECHO Push failed. 
+) ELSE (
+    ECHO Push succeeded.
+)
