5d1c5aa6669652c6b8edfe951add236c830625fe
Christian Holmstrand <christian.holmstrand@starcounter.com>
2017-05-24 12:44:34 +0200

    Added and enabled the correct url to myget for pushing.

--- build/push.bat
+++ build/push.bat
@@ -1,9 +1,9 @@
 @ECHO OFF
 
-:: SET "NUGET_URL=https://www.myget.org/F/starcounter/api/v2/package"
+SET "NUGET_URL=https://www.myget.org/F/starcounter/api/v2/package"
 :: SET "NUGET_SS_URL=https://www.myget.org/F/starcounter/symbols/api/v2/package"
 
-SET "NUGET_URL=\\127.0.0.1\Share"
+::SET "NUGET_URL=\\127.0.0.1\Share"
 :: SET "NUGET_SS_URL=\\127.0.0.1\Share"
 
 IF "%StarcounterNuGetKeyFull%"=="" (
