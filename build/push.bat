b00d79fca440b5ce51730b377ba2137af8b81b1c
Christian Holmstrand <christian.holmstrand@starcounter.com>
2017-05-30 20:10:15 +0200

    Changed name of environment variable containing myget API key.

--- build/push.bat
+++ build/push.bat
@@ -3,15 +3,12 @@
 SET "NUGET_URL=https://www.myget.org/F/starcounter/api/v2/package"
 :: SET "NUGET_SS_URL=https://www.myget.org/F/starcounter/symbols/api/v2/package"
 
-::SET "NUGET_URL=\\127.0.0.1\Share"
-:: SET "NUGET_SS_URL=\\127.0.0.1\Share"
-
-IF "%StarcounterNuGetKeyFull%"=="" (
-	ECHO StarcounterNuGetKeyFull is not set
-	EXIT /b 990
+IF "%MYGET_API_KEY%"=="" (
+	ECHO Myget API key not set. Skipping push.
+	EXIT /b 0
 )
 
-dotnet nuget push --source %NUGET_URL% --api-key %StarcounterNuGetKeyFull%  ..\artifacts\
+dotnet nuget push --source %NUGET_URL% --api-key %MYGET_API_KEY%  ..\artifacts\
 
 IF ERRORLEVEL 1 (
     ECHO Push failed. 
