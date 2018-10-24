c6a927aa6f4bd51a30582a9b35a4fa03652fce0d
Mackiovello <elandroe@icloud.com>
2018-10-24 19:30:20 +0800

    Add more tests for ExceptionFactory

--- test/Starcounter.ErrorCodes.Tests/ExceptionFactoryTests.cs
+++ test/Starcounter.ErrorCodes.Tests/ExceptionFactoryTests.cs
@@ -1,6 +1,7 @@
 namespace Starcounter.ErrorCodes.Tests
 {
     using System;
+    using System.IO;
     using Xunit;
 
     public class ExceptionFactoryTests
@@ -20,6 +21,75 @@ namespace Starcounter.ErrorCodes.Tests
                 var e = Assert.Throws<ArgumentNullException>(createException);
                 Assert.NotEmpty(e.Message);
                 Assert.NotEmpty(e.ParamName);
+            }
+
+            [Fact]
+            public void ExceptionHasMessageCreatedByMessageFactory()
+            {
+                //Given
+                var factory = new ExceptionFactory();
+                var message = "MyMessage";
+                Func<uint, string, object[], string> messageFactory = (x, y, z) => message;
+
+                //When
+                var exception = factory.CreateException(0, null, null, messageFactory);
+
+                //Then
+                Assert.Equal(message, exception.Message);
+            }
+
+            [Fact]
+            public void ErrorCode3000CreatesIOException()
+            {
+                //Given
+                uint errorCode = 3000;
+                var factory = new ExceptionFactory();
+
+                //When
+                var exception = factory.CreateException(errorCode, null, null, (x, y, z) => "message");
+
+                //Then
+                Assert.IsType<IOException>(exception);
+            }
+
+            [Fact]
+            public void HadInnerExceptionSetIfInnerExceptionIsPassed()
+            {
+                //Given
+                var innerException = new Exception();
+                var factory = new ExceptionFactory();
+
+                //When
+                var exception = factory.CreateException(0, innerException, null, (y, z, x) => "message");
+
+                //Then
+                Assert.Equal(innerException, exception.InnerException);
+            }
+
+            [Fact]
+            public void HelpLinkIsSetForException()
+            {
+                //Given
+                var factory = new ExceptionFactory();
+
+                //When
+                var exception = factory.CreateException(0, null, null, (x, y, z) => "message");
+
+                //Then
+                Assert.NotEmpty(exception.HelpLink);
+            }
+
+            [Fact]
+            public void SourceIsSetForException()
+            {
+                //Given
+                var factory = new ExceptionFactory();
+
+                //When
+                var exception = factory.CreateException(0, null, null, (x, y, z) => "message");
+
+                //Then
+                Assert.NotEmpty(exception.Source);
             }
         }
     }
